5961f7eb3578ab26597fe54f2df1a80a9deb0d6b
Asad Sayeed <asayeed@mbl.ca>
2018-09-30 18:56:44 +0200

    created basic training script outline

---
+++ train.py
@@ -0,0 +1,56 @@
+# This is the main training script that we should be able to run to grade
+# your model training for the assignment.
+# You can create whatever additional modules and helper scripts you need,
+# as long as all the training functionality can be reached from this script.
+
+# Add/update whatever imports you need.
+from argparse import ArgumentParser
+
+# If you do option A, you may want to place your code here.  You can
+# update the arguments as you need.
+def optA():
+    print("Option A not implemented!")
+
+# If you do option B, you may want to place your code here.  You can
+# update the arguments as you need.
+def optB():
+    print("Option B not implemented!")
+
+# Modify this as needed.
+if __name__ == "__main__":
+    parser = ArgumentParser("Train a model.")    
+    # Add your own options as flags HERE as necessary (and some will be necessary!).
+    # You shouldn't touch the arguments below.
+    parser.add_argument('-P', '--option', type=str,
+                        help="Either A or B, based on the version of the assignment you want to run. (REQUIRED)",
+                        required=True)
+    parser.add_argument('-m', '--maxinstances', type=int,
+                        help="The maximum number of instances to be processed per category. (REQUIRED)",
+                        required=True)
+    parser.add_argument('checkpointdir', type=str,
+                        help="directory for storing checkpointed models and other metadata (recommended to create a directory under /scratch/)")
+    parser.add_argument('modelfile', type=str, help="output model file")
+    parser.add_argument('categories', metavar='cat', type=str, nargs='+',
+                        help='two or more COCO category labels')
+    args = parser.parse_args()
+
+    print("Output model in " + args.modelfile)
+    print("Working directory at " + args.checkpointdir)
+    print("Maximum instances is " + str(args.maxinstances))
+
+    if len(args.categories) < 2:
+        print("Too few categories (<2).")
+        exit(0)
+
+    print("The queried COCO categories are:")
+    for c in args.categories:
+        print("\t" + c)
+
+    print("Executing option " + args.option)
+    if args.option == 'A':
+        optA()
+    elif args.option == 'B':
+        optB()
+    else:
+        print("Option does not exist.")
+        exit(0)
