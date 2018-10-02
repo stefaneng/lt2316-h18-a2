ced2c3ecd3f19cda4ac5a726bc96cd219f79ae1b
Asad Sayeed <asayeed@mbl.ca>
2018-10-02 15:50:33 +0200

    scripts ready to go

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-# This is the main training script that we should be able to run to grade
+# This is the main testing script that we should be able to run to grade
 # your model training for the assignment.
 # You can create whatever additional modules and helper scripts you need,
 # as long as all the training functionality can be reached from this script.
@@ -10,18 +10,18 @@ import mycoco
 # If you do option A, you may want to place your code here.  You can
 # update the arguments as you need.
 def optA():
-    mycoco.setmode('train')
+    mycoco.setmode('test')
     print("Option A not implemented!")
 
 # If you do option B, you may want to place your code here.  You can
 # update the arguments as you need.
 def optB():
-    mycoco.setmode('train')
+    mycoco.setmode('test')
     print("Option B not implemented!")
 
 # Modify this as needed.
 if __name__ == "__main__":
-    parser = ArgumentParser("Train a model.")    
+    parser = ArgumentParser("Evaluate a model.")    
     # Add your own options as flags HERE as necessary (and some will be necessary!).
     # You shouldn't touch the arguments below.
     parser.add_argument('-P', '--option', type=str,
@@ -30,24 +30,11 @@ if __name__ == "__main__":
     parser.add_argument('-m', '--maxinstances', type=int,
                         help="The maximum number of instances to be processed per category. (optional)",
                         required=False)
-    parser.add_argument('checkpointdir', type=str,
-                        help="directory for storing checkpointed models and other metadata (recommended to create a directory under /scratch/)")
-    parser.add_argument('modelfile', type=str, help="output model file")
-    parser.add_argument('categories', metavar='cat', type=str, nargs='+',
-                        help='two or more COCO category labels')
+    parser.add_argument('modelfile', type=str, help="model file to evaluate")
     args = parser.parse_args()
 
     print("Output model in " + args.modelfile)
-    print("Working directory at " + args.checkpointdir)
     print("Maximum instances is " + str(args.maxinstances))
-
-    if len(args.categories) < 2:
-        print("Too few categories (<2).")
-        exit(0)
-
-    print("The queried COCO categories are:")
-    for c in args.categories:
-        print("\t" + c)
 
     print("Executing option " + args.option)
     if args.option == 'A':
